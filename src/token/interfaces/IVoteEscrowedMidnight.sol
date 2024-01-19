093ae9899bbc65dad19c97959a48bcd684341cc2
Aaron <155360854+otfen@users.noreply.github.com>
2024-01-19 19:16:02 +1030

    feat: create vote escrow interface

---
+++ src/token/interfaces/IVoteEscrowedMidnight.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.20;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+interface IVoteEscrowedMidnight is IERC20, IERC20Permit {
+    /// @notice Thrown when a caller lacks permission to perform an action.
+    error Forbidden();
+
+    /// @notice A structure representing an unlocking lock.
+    /// @param value The amount of tokens locked.
+    /// @param timestamp The starting timestamp of the unlocking period.
+    struct UnlockingLock {
+        uint256 value;
+        uint256 timestamp;
+    }
+
+    /// @notice Returns the voting power of an account.
+    /// @param account The account to retrieve the voting power for.
+    function weight(address account) external view returns (uint256);
+
+    /// @notice Returns the weight of an unlocking lock.
+    /// @param id The lock identifier.
+    function lockWeight(uint256 id) external view returns (uint256);
+
+    /// @notice Returns the unlocking locks owned by an account.
+    /// @param account The account to retrieve unlocking locks for.
+    function locks(address account) external view returns (uint256[] memory);
+
+    /// @notice Locks governance tokens.
+    /// @param amount The amount of governance tokens to lock.
+    function lock(uint256 amount) external;
+
+    /// @notice Starts the unlocking period of vote escrowed governance tokens.
+    /// @param amount The amount of vote escrowed governance tokens to unlock.
+    function unlock(uint256 amount) external;
+
+    /// @notice Relocks unlocking governance tokens.
+    /// @param id The lock identifier.
+    function relock(uint256 id) external;
+
+    /// @notice Claims unlocked governance tokens.
+    /// @param id The lock identifier.
+    function claim(uint256 id) external;
+}
